08f911e5a1bd9e1f03f6bd8c705dcd30ab682695
Mr-SK534 <ksharad534@gmail.com>
2026-07-28 16:31:16 +0530

    fixing checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,8 @@
-export const cart=[{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 1,
-},{
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 2
-}];
+export let cart=JSON.parse(localStorage.getItem('cart'));
+
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId, quantitySelected){
     let matchingItem;
@@ -22,4 +20,18 @@ export function addToCart(productId, quantitySelected){
             quantity: quantitySelected
         });
     }
+    saveToStorage();
+}
+
+export function removeFromCart(productId){
+    const newCart=[];
+
+    cart.forEach((cartItem)=>{
+        if(cartItem.productId !== productId){
+            newCart.push(cartItem);
+        }
+    });
+     cart = newCart;
+
+     saveToStorage();
 }
